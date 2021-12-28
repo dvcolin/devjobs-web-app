d5a0cdb8fc572f57525730e631f41c233b102e8d
dvcolin <colin@colindevries.dev>
2021-12-27 23:24:14 -0800

    saved theme selection in local storage. added button to toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
 import Layout from "./components/layout/Layout";
 import JobList from "./components/JobList";
 import data from "./data.json";
 
 const App = () => {
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (!storedTheme) {
+      localStorage.setItem("theme", "light");
+    }
+
+    const root = document.documentElement;
+    root.setAttribute("data-theme", localStorage.getItem("theme")!);
+  }, []);
+
   return (
     <Layout>
       <JobList jobs={data} />
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,12 +2,21 @@ import "../../scss/components/layout/Header.scss";
 import logo from "../../assets/desktop/logo.svg";
 
 const Header = () => {
+  const toggleTheme = () => {
+    const root = document.documentElement;
+    const currentTheme = root.getAttribute("data-theme");
+    const theme = currentTheme === "light" ? "dark" : "light";
+
+    root.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  };
   return (
     <header className="header">
       <div className="header__container">
         <div className="header__logo-box">
           <img src={logo} alt="devjobs logo" className="header__logo" />
         </div>
+        <button onClick={toggleTheme}>Toggle theme</button>
       </div>
     </header>
   );
