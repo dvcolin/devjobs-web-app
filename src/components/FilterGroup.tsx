b3d5243267fa900665ca6d17aeb5917ab5d890f1
dvcolin <colin@colindevries.dev>
2022-01-14 13:30:08 -0500

    created responsive styles for FilterGroup component

--- src/components/FilterGroup.tsx
+++ src/components/FilterGroup.tsx
@@ -1,7 +1,10 @@
 import { useContext } from "react";
 import "../scss/components/FilterGroup.scss";
 import filterIcon from "../assets/mobile/icon-filter.svg";
-import searchIcon from "../assets/mobile/icon-search.svg";
+import searchIconMobile from "../assets/mobile/icon-search.svg";
+import searchIconDesktop from "../assets/desktop/icon-search.svg";
+import locationIcon from "../assets/desktop/icon-location.svg";
+import checkIcon from "../assets/desktop/icon-check.svg";
 import SearchContext from "../contexts/SearchContext";
 
 interface FilterGroupProps {
@@ -11,7 +14,7 @@ interface FilterGroupProps {
 
 const FilterGroup = ({ filterJobs, setModalOpen }: FilterGroupProps) => {
   const [searchFields, setSearchFields] = useContext(SearchContext);
-  const { queryString } = searchFields;
+  const { queryString, location, fullTime } = searchFields;
 
   return (
     <div className="filter-group">
@@ -22,9 +25,14 @@ const FilterGroup = ({ filterJobs, setModalOpen }: FilterGroupProps) => {
           filterJobs();
         }}
       >
-        <div className="filter-group__title-box">
+        <div className="filter-group__item-box filter-group__title-box">
+          <img
+            src={searchIconDesktop}
+            alt="Search icon"
+            className="filter-group__item-icon filter-group__title-icon"
+          />
           <input
-            className="filter-group__title-input"
+            className="filter-group__item-input"
             placeholder="Filter by title..."
             value={queryString}
             onChange={(e) => {
@@ -35,6 +43,51 @@ const FilterGroup = ({ filterJobs, setModalOpen }: FilterGroupProps) => {
             }}
           />
         </div>
+        <div className="filter-group__item-box filter-group__location-box">
+          <img
+            src={locationIcon}
+            alt="Location icon"
+            className="filter-group__item-icon"
+          />
+          <input
+            className="filter-group__item-input"
+            placeholder="Filter by location..."
+            value={location}
+            onChange={(e) => {
+              setSearchFields({
+                ...searchFields,
+                location: e.target.value,
+              });
+            }}
+          />
+        </div>
+        <div className="filter-group__item-box filter-group__full-time-box">
+          <label className="filter-group__full-time-label">
+            <input
+              type="checkbox"
+              className="filter-group__full-time-input"
+              checked={fullTime}
+              onChange={() => {
+                setSearchFields({
+                  ...searchFields,
+                  fullTime: !fullTime,
+                });
+              }}
+            />
+            <span className="checkbox">
+              <img className="checkbox-icon" src={checkIcon} alt="Check icon" />
+            </span>
+            <span className="filter-group__full-time-label-text">
+              Full Time{" "}
+              <span className="filter-group__full-time-label-text-only">
+                Only
+              </span>
+            </span>
+          </label>
+        </div>
+
+        <button className="filter-group__search-button">Search</button>
+
         <div className="filter-group__icon-buttons">
           <button
             type="button"
@@ -47,7 +100,7 @@ const FilterGroup = ({ filterJobs, setModalOpen }: FilterGroupProps) => {
             type="submit"
             className="filter-group__icon-button filter-group__icon-button--search"
           >
-            <img src={searchIcon} alt="Search" />
+            <img src={searchIconMobile} alt="Search" />
           </button>
         </div>
       </form>
